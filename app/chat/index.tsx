a2d4cbe1251b50fa0f87b9cedcf6442ea181f851
Vadim Savin <savinvadim1312@gmail.com>
2023-07-07 18:02:47 +0100

    Private messages

--- app/chat/index.tsx
+++ app/chat/index.tsx
@@ -1,10 +1,20 @@
 import { useRouter } from 'expo-router';
 import { ChannelList } from 'stream-chat-expo';
+import { useAuth } from '../../src/context/auth';
 
 const ChatScreen = () => {
   const router = useRouter();
+  const { user } = useAuth();
+
+  const isPrivate = {
+    type: 'messaging',
+    members: { $in: [user.id.toString()] },
+  };
+  const isPublic = { type: 'livestream' };
+
   return (
     <ChannelList
+      filters={{ $or: [isPrivate, isPublic] }}
       onSelect={(channel) => router.push(`/chat/channel/${channel.id}`)}
     />
   );
--- src/components/UserListItem.tsx
+++ src/components/UserListItem.tsx
@@ -1,9 +1,25 @@
-import { View, Text } from 'react-native';
-import { User } from '../context/auth';
+import { View, Text, Pressable } from 'react-native';
+import { User, useAuth } from '../context/auth';
+import { useChatContext } from 'stream-chat-expo';
+import { useRouter } from 'expo-router';
 
 const UserListItem = ({ user }: { user: User }) => {
+  const { client } = useChatContext();
+  const { user: me } = useAuth();
+  const router = useRouter();
+
+  const startChannel = async () => {
+    const channel = client.channel('messaging', {
+      members: [user.id.toString(), me.id.toString()],
+    });
+    await channel.watch();
+
+    router.push(`/chat/channel/${channel.id}`);
+  };
+
   return (
-    <View
+    <Pressable
+      onPress={startChannel}
       style={{
         backgroundColor: 'white',
         margin: 5,
@@ -13,7 +29,7 @@ const UserListItem = ({ user }: { user: User }) => {
       }}
     >
       <Text>{user.name}</Text>
-    </View>
+    </Pressable>
   );
 };
 
